1c62a1ba337e8379748540834ceb57c801d1ce7f
Michael Esteban <mickel13@gmail.com>
2024-09-25 16:12:14 +1000

    initial commit

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,73 @@
+import { TWITCH_CLIENT_ID, TWITCH_CLIENT_SECRET } from '$env/static/private';
+
+export async function load({ params }) {
+	async function getOAuthToken() {
+		const clientId = TWITCH_CLIENT_ID;
+		const clientSecret = TWITCH_CLIENT_SECRET;
+
+		const response = await fetch('https://id.twitch.tv/oauth2/token', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded'
+			},
+			body: new URLSearchParams({
+				client_id: clientId,
+				client_secret: clientSecret,
+				grant_type: 'client_credentials'
+			})
+		});
+
+		const data = await response.json();
+		return data.access_token;
+	}
+
+	async function getCategoryId(oauthToken) {
+		const response = await fetch(
+			`https://api.twitch.tv/helix/games?name=Software%20and%20Game%20Development&period=week&type=archive`,
+			{
+				headers: {
+					'Client-ID': TWITCH_CLIENT_ID,
+					Authorization: `Bearer ${oauthToken}`
+				}
+			}
+		);
+
+		const data = await response.json();
+		return data.data[0].id;
+	}
+
+	async function getVODsByCategory(categoryId, oauthToken) {
+		const url = `https://api.twitch.tv/helix/videos?game_id=${categoryId}&sort=views&period=week&type=archive&language=en`;
+
+		const response = await fetch(url, {
+			headers: {
+				Authorization: `Bearer ${oauthToken}`,
+				'Client-Id': TWITCH_CLIENT_ID,
+				'accept-language': 'YOUWHAT' // https://github.com/twitchdev/issues/issues/651 lol
+			}
+		});
+
+		const data = await response.json();
+		console.log('VOD data:', JSON.stringify(data, null, 2));
+		return data.data;
+	}
+
+	const oauthToken = await getOAuthToken();
+	const categoryId = await getCategoryId(oauthToken);
+	const vods = await getVODsByCategory(categoryId, oauthToken);
+
+	const formattedVods = vods.map((vod) => {
+		return {
+			id: vod.id,
+			title: vod.title,
+			thumbnail: vod.thumbnail_url.replace('%{width}', '320').replace('%{height}', '180'),
+			createdAt: vod.created_at,
+			username: vod.user_name,
+			views: vod.view_count,
+			duration: vod.duration,
+			url: vod.url
+		};
+	});
+
+	return { vods: formattedVods };
+}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	interface Video {
+		id: string;
+		title: string;
+		thumbnail: string;
+		createdAt: string;
+		username: string;
+		views: number;
+		duration: string;
+		url: string;
+	}
+
+	export let data: { vods: Video[] };
+</script>
+
+<h1>Better VOD Gallery</h1>
+{#each data.vods as video}
+	<div>
+		<a href={video.url}>
+			<img src={video.thumbnail} alt={video.title} />
+		</a>
+		<p>{video.title}</p>
+		<p>{video.username}</p>
+		<p>{video.views} views</p>
+		<p>{video.duration}</p>
+		<p>{video.createdAt}</p>
+	</div>
+{/each}
